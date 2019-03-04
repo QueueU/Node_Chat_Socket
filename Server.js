59622e5c2b53244cb9777f69e91038b4e4a9c14b
Ujash <17bce161@nirmauni.ac.in>
2019-03-04 16:37:44 +0530

    T3

--- Server.js
+++ Server.js
@@ -1,1 +1,38 @@
-const express =require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+
+const cookieParser =require('cookie-parser');
+const validator = require('')
+
+container.resolve(function (users) {
+    const app = SetupExprss();
+
+    function SetupExprss() {
+        const app = express();
+
+        const server = http.createServer(app);
+        server.listen(3000, function () {
+            console.log('Listing on port  3000');
+        });
+
+        ConfigureExpress(app);
+        const router = require('express-promise-router')();
+
+        users.SetRouting(router);
+        app.use(router);
+    }
+
+
+
+    function ConfigureExpress(app) {
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+    }
+
+})
--- container.js
+++ container.js
@@ -3,11 +3,21 @@ const path=require('path');
 const container =dependable.container();
 
 const simpleDependecies = [
-    ['_','lodash']
+    ['_','lodash'],
+    ['async','async']
 ];
 
 simpleDependecies.forEach(function(val){
-    container.register(val[0],function()){
+    container.register(val[0],function(){
         return require(val[1]);
-    }
-})
+    });
+});
+
+container.load(path.join(__dirname,'/controllers'));
+container.load(path.join(__dirname,'/helpers'));
+
+container.register('container',function(){
+    return container;
+});
+
+module.exports=container;
