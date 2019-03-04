4c0272182ef9dbf7734bbad95f6b70666b396670
Ujash <17bce161@nirmauni.ac.in>
2019-03-04 17:48:12 +0530

    Local Host not working but after comment store: new MongoStore all are working perfectly...but why this Store not working thatt i dont know

--- Server.js
+++ Server.js
@@ -6,9 +6,21 @@ const container = require('./container');
 
 
 const cookieParser =require('cookie-parser');
-const validator = require('')
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('flash');
+const passport = require('passport');
+const mongoose=require('mongoose');
+
 
 container.resolve(function (users) {
+
+    
+    mongoose.Promise =global.Promise;
+    mongoose.connect=('mongodb://localhost/Chat_App',{useMongoClient:true});  
+    
+
     const app = SetupExprss();
 
     function SetupExprss() {
@@ -30,9 +42,25 @@ container.resolve(function (users) {
 
     function ConfigureExpress(app) {
         app.use(express.static('public'));
+
+
+       app.use(cookieParser());
         app.set('view engine', 'ejs');
-        app.use(bodyParser.json());
+       app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
+
+        app.use(validator());
+        
+         app.use(session({
+            secret: 'addyourownsecretkey',
+            resave: false,
+            saveUninitialized: false,
+            //store: new MongoStore({mongooseConnection: mongoose.connection})
+        })); 
+
+      app.use(flash());
+       app.use(passport.initialize());
+       app.use(passport.session());
     }
 
-})
+});
--- controllers/users.js
+++ controllers/users.js
@@ -5,11 +5,20 @@ module.exports=function(_){
     return {
         SetRouting :function(router){
             router.get('/',this.indexPage);
+            router.get('/signup',this.getSignUp);
+
         },
 
         indexPage: function (req,res)
         {
             return res.render('index',{test:'this is test'});
+        },
+
+        getSignUp: function(req,res)
+        {
+                return res.render('signup');
         }
+
+
     }
 }
